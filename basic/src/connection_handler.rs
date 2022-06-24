4184b37fe68f52b40fff7cdcbcda23940d311c64
Mattia Bradascio <28816406+bredamatt@users.noreply.github.com>
2022-06-24 16:05:12 +0100

    add basic 200 result

--- basic/src/connection_handler.rs
+++ basic/src/connection_handler.rs
@@ -13,7 +13,10 @@ pub mod connection_handler {
         pub fn handle_connection(&self, mut stream: TcpStream) {
             let mut buffer = [0; 1024];
             stream.read(&mut buffer).unwrap();
-            println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+            // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+            let response = "HTTP/1.1 200 OK\r\n\r\n";
+            stream.write(response.as_bytes()).unwrap();
+            stream.flush().unwrap();
         }
     }
 }
