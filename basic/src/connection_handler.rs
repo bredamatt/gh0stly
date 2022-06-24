198d7f9879a4b9c14954f67f43dafc1569b957af
Mattia Bradascio <28816406+bredamatt@users.noreply.github.com>
2022-06-24 17:28:42 +0100

    Add hello

--- basic/src/connection_handler.rs
+++ basic/src/connection_handler.rs
@@ -1,9 +1,9 @@
 pub mod connection_handler {
+    use std::fs;
     use std::net::TcpStream;
     use std::io::prelude::*;
     
-    pub struct ConnectionHandler {
-    }
+    pub struct ConnectionHandler {}
     
     impl ConnectionHandler {
         pub fn new() -> ConnectionHandler {
@@ -14,7 +14,15 @@ pub mod connection_handler {
             let mut buffer = [0; 1024];
             stream.read(&mut buffer).unwrap();
             // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-            let response = "HTTP/1.1 200 OK\r\n\r\n";
+            
+            
+            let contents = fs::read_to_string("hello.html").unwrap();
+            
+            let response = format!(
+                "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+                contents.len(), contents
+            );
+
             stream.write(response.as_bytes()).unwrap();
             stream.flush().unwrap();
         }
